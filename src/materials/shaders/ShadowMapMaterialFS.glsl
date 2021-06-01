5468d53270ae3329aabf27255c9122d9ec4536b4
mbredif <mathieu.bredif@ign.fr>
2021-06-01 14:20:10 +0200

    gh-pages-deploy

--- src/materials/shaders/ShadowMapMaterialFS.glsl
+++
@@ -1,8 +0,0 @@
-varying vec4 vPositionImage;
-
-void main() {
-
-  float distanceCamera = ((vPositionImage.z / vPositionImage.w) + 1.) / 2.;
-  gl_FragColor = vec4(vec3(distanceCamera), 1.);
-
-}
--- src/parsers/parsers.js
+++
@@ -1,6 +0,0 @@
-export { default as BundlerParser } from './BundlerParser';
-export { default as GeoJSONParser } from './GeoJSONParser';
-// export { default as GMLParser } from './GMLParser';
-export { default as MatisParser } from './MatisParser';
-export { default as MicmacParser } from './MicmacParser';
-export { default as OPKParser } from './OPKParser';
